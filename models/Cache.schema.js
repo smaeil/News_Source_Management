14a95924b1b8e3935add94693de9172cbe57e159
Smaeil Ahmadi <smaeilahmadi@mac-home.local>
2026-04-23 21:33:38 +0200

    cache model updated now can full text search

--- models/Cache.schema.js
+++ models/Cache.schema.js
@@ -1,15 +1,20 @@
 import mongoose from "mongoose";
 
 const cacheSchema = new mongoose.Schema({
-    sourceId: { 
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: 'Source', 
-        required: true,
-        unique: true 
-    },
-    articles: { type: Array, required: true },
-    createdAt: { type: Date, default: Date.now}
+    sourceId: { type: mongoose.Schema.Types.ObjectId, ref: 'Source', unique: true },
+    articles: [{
+        title: String,
+        content: String,
+        link: String,
+        thumbnail: String,
+        publishDate: Date
+    }],
+    updatedAt: { type: Date, default: Date.now }
 });
 
+// Create a Text Index on title and content for Full-Text Search
+cacheSchema.index({ "articles.title": "text", "articles.content": "text" });
+
 const Cache = mongoose.model("Cache", cacheSchema);
+
 export default Cache;
